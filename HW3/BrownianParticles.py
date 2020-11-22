46e84c7ac19f1d870a10cbdfffaa2c0d2d387a06
Arwin <arwinsaleh@hotmail.com>
2020-11-22 23:12:57 +0100

    commit

--- HW3/BrownianParticles.py
+++ HW3/BrownianParticles.py
@@ -4,7 +4,7 @@ from numpy.lib.function_base import append
 import math
 
 class BrownianParticles:
-    def __init__(self, nr_particles, grid_length, nr_steps, D_T, D_R, v):
+    def __init__(self, nr_particles, grid_length, nr_steps, D_T, D_R, v, tau):
         self.nr_particles = nr_particles
         self.nr_steps = nr_steps
         self.grid_length = grid_length
@@ -16,13 +16,17 @@ class BrownianParticles:
         self.particle_historyY = list()
         self.historyX = [[] for i in range(0, nr_particles)]
         self.historyY = [[] for i in range(0, nr_particles)]
+        self.historyMSD = [[] for i in range(0, nr_particles)]
         self.D_T = D_T
         self.D_R = D_R
         self.theta = np.ones((nr_particles, 1))
         self.v = v
+        self.tau = tau
+        self.time_step = 1
 
     def draw_particles(self):
         plt.clf()
+        plt.title('t = ' + str(self.time_step))
         for i in range(self.nr_particles):
             if len(self.historyX) > 1:
                 plt.plot(self.historyX[i], self.historyY[i], '-o', markevery=[len(self.particle_historyX)-1])
@@ -44,6 +48,12 @@ class BrownianParticles:
         self.X[i] += self.velocities[i, 0]
         self.Y[i] += self.velocities[i, 1]
 
+    def compute_MSD(self, i):
+        MSD = 0
+        if i < self.nr_particles - 1:
+            MSD = ( (self.X[i + self.tau] - self.X[i]) ** 2 + (self.Y[i + self.tau] - self.Y[i]) ** 2 ) / self.time_step
+        return MSD
+
     def step(self):
         for step in range(self.nr_steps):
             self.draw_particles()
@@ -53,9 +63,18 @@ class BrownianParticles:
                 self.update_position(i)
                 self.historyX[i].append(float(self.X[i]))
                 self.historyY[i].append(float(self.Y[i]))
+                self.historyMSD[i].append(self.compute_MSD(i))
+            self.time_step += 1
 
 def main():
-    brown = BrownianParticles(nr_particles=20, grid_length=50, nr_steps=1000, D_T=0.5, D_R=0.5, v=0.1)
+    brown = BrownianParticles(nr_particles=20, grid_length=10, nr_steps=10, D_T=0.5, D_R=0.5, v=0.1, tau=1)
     brown.step()
+    t = np.zeros((brown.nr_steps, 1))
+    for i in range(1, brown.nr_steps):
+        t[i] = i+1
+    plt.figure()
+    for i in range(1, brown.nr_particles):
+        plt.plot(np.log(t), np.log(brown.historyMSD[i]))
+    plt.show()
 
 main()

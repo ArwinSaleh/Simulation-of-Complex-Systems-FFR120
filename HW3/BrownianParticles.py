e08453a2c8412d12b5b92509df8888b0bc983582
Arwin <arwinsaleh@hotmail.com>
2020-11-22 22:05:50 +0100

    working model

--- HW3/BrownianParticles.py
+++ HW3/BrownianParticles.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy.lib.function_base import append
+import math
 
 class BrownianParticles:
     def __init__(self, nr_particles, grid_length, nr_steps, D_T, D_R, v):
@@ -13,43 +14,48 @@ class BrownianParticles:
         self.velocities = np.zeros((nr_particles, 2))
         self.particle_historyX = list()
         self.particle_historyY = list()
-        self.particle_historyX.append(0)
-        self.particle_historyY.append(0)
+        self.historyX = [[] for i in range(0, nr_particles)]
+        self.historyY = [[] for i in range(0, nr_particles)]
         self.D_T = D_T
         self.D_R = D_R
-        self.theta = np.zeros((nr_particles, 1))
+        self.theta = np.ones((nr_particles, 1))
         self.v = v
 
     def draw_particles(self):
         plt.clf()
-        plt.plot(self.particle_historyX, self.particle_historyY, "r-")
-        plt.axis([-self.grid_length, self.grid_length, -self.grid_length, self.grid_length])
+        for i in range(self.nr_particles):
+            if len(self.historyX) > 1:
+                plt.plot(self.historyX[i], self.historyY[i], '-o', markevery=[len(self.particle_historyX)-1])
+        plt.axis('equal')
         plt.draw()
-        plt.pause(0.0001)
-
+        plt.pause(0.00000001)
 
     def update_velocity(self, i):
         Wx = np.random.normal(0, 1)
         Wy = np.random.normal(0, 1)
-        self.velocities[i, 0] += Wx * np.sqrt(2 * self.D_T)
-        self.velocities[i, 1] += Wy * np.sqrt(2 * self.D_T)
+        self.velocities[i, 0] = self.v * math.cos(self.theta[i]) + Wx * math.sqrt(2 * self.D_T)
+        self.velocities[i, 1] = self.v * math.sin(self.theta[i]) + Wy * math.sqrt(2 * self.D_T)
     
     def update_theta(self, i):
         Wo = np.random.normal(0, 1)
-        self.theta
+        self.theta[i] = -Wo * math.sqrt(2 * self.D_R)
     
     def update_position(self, i):
         self.X[i] += self.velocities[i, 0]
         self.Y[i] += self.velocities[i, 1]
-        self.particle_historyX.append(self.X[i])
-        self.particle_historyY.append(self.Y[i])
 
     def step(self):
         for step in range(self.nr_steps):
             self.draw_particles()
             for i in range(self.nr_particles):
+                self.update_theta(i)
                 self.update_velocity(i)
                 self.update_position(i)
+                self.historyX[i].append(float(self.X[i]))
+                self.historyY[i].append(float(self.Y[i]))
 
-brown = BrownianParticles(nr_particles=1, grid_length=100, nr_steps=1000, D_T=0.2, D_R=0.5, v=0.5)
-brown.step()
+def main():
+    brown = BrownianParticles(nr_particles=50, grid_length=100, nr_steps=1000, D_T=5, D_R=5, v=10)
+    brown.step()
+
+main()

12359f6efabe6e6dbd5199570ab4d993897d96fd
Arwin <arwinsaleh@hotmail.com>
2020-11-25 19:24:23 +0100

    commit

--- HW3/BrownianParticles.py
+++ HW3/BrownianParticles.py
@@ -1,4 +1,5 @@
 from math import log
+from re import T
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy.core.fromnumeric import size
@@ -22,13 +23,22 @@ class BrownianParticles:
         self.particle_historyY = list()
         self.historyX = [[] for i in range(0, nr_particles)]
         self.historyY = [[] for i in range(0, nr_particles)]
+        self.X_history = list()
+        self.Y_history = list()
         self.historyMSD = [[] for i in range(0, nr_particles)]
         self.D_T = D_T
-        self.D_R = D_Ri
+        self.D_R = D_R
+        self.T0 = T0
+        self.r_c=r_c
+        self.theta = np.random.uniform(-1, 1, size=(nr_particles, 1))
+        self.v = np.random.uniform(-0.00001, 0.00001, size=(nr_particles, 1))
         self.tau = tau
         self.time_step = 1
         self.DRAW = DRAW
         self.DRAW_PATH = DRAW_PATH
+
+    def initialize_inactive_particles(self):
+        self.active_particles[int(0.2 * self.nr_particles) : self.nr_particles] = 0
 
     def draw_particles(self, LEGEND):
         plt.clf()
@@ -37,7 +47,8 @@ class BrownianParticles:
             for i in range(self.nr_particles):
                     plt.plot(self.historyX[i], self.historyY[i], '.', markevery=[len(self.particle_historyX)-1], label='v = ' + str(self.v[i]) + " \u03BCm/s")
         else:
-            plt.plot(self.X, self.Y, '.', markersize=10)
+            plt.plot(self.X[np.where(self.active_particles==1)], self.Y[np.where(self.active_particles==1)], 'o')
+            plt.plot(self.X[np.where(self.active_particles==0)], self.Y[np.where(self.active_particles==0)], 'o')
 
         if LEGEND:
             plt.legend()
@@ -64,22 +75,29 @@ class BrownianParticles:
         self.theta[i] = self.theta[i] + Wo * math.sqrt(2 * self.D_R)
     
     def update_position(self, i):
-
-        self.X[i] += self.velocities[i, 0]
-        self.Y[i] += self.velocities[i, 1]
-        if self.X[i] > self.grid_length:
-            self.X[i] -= self.velocities[i, 0]
-        if self.Y[i] > self.grid_length:
-            self.Y[i] -= self.velocities[i, 1]
-        if self.X[i] < 0:
-            self.X[i] -= self.velocities[i, 0]
-        if self.Y[i] < 0:
-            self.Y[i] -= self.velocities[i, 1]
-        if self.collision(i):
-            self.X[i] -= self.velocities[i, 0]
-            self.Y[i] -= self.velocities[i, 1]
+        if self.active_particles[i] == 1:
+            self.X[i] += self.velocities[i, 0]
+            self.Y[i] += self.velocities[i, 1]
+            if self.X[i] > self.grid_length:
+                self.X[i] -= self.velocities[i, 0]
+            if self.Y[i] > self.grid_length:
+                self.Y[i] -= self.velocities[i, 1]
+            if self.X[i] < 0:
+                self.X[i] -= self.velocities[i, 0]
+            if self.Y[i] < 0:
+                self.Y[i] -= self.velocities[i, 1]
+        (collided, particles_on_site) = self.collision(i)
+        if collided and self.time_step > 2:
+            self.X[particles_on_site] += self.velocities[i, 0]
+            self.Y[particles_on_site] += self.velocities[i, 1]
         self.historyX[i].append(float(self.X[i]))
         self.historyY[i].append(float(self.Y[i]))
+
+    def inactive_noise(self, noise_factor):
+        r = np.random.uniform(-noise_factor, noise_factor)
+
+        self.X[np.where(self.active_particles==0)] += r
+        self.Y[np.where(self.active_particles==0)] += r
 
     def compute_MSD(self, i, step):
         MSD = 0
@@ -91,37 +109,39 @@ class BrownianParticles:
         self.historyMSD[i].append(MSD)
     
     def compute_torque(self, n):
-        inactive_particles = np.where(self.active_particles == 0)[0]
-        active_X = np.delete(self.X, inactive_particles)
-        active_Y = np.delete(self.Y, inactive_particles)
-        active_X = np.delete(self.X, 0)
-        active_Y = np.delete(self.Y, 0)
+        active_X = np.delete(self.X, np.where(self.X==self.X[n]))   # n =/= i
+        active_Y = np.delete(self.Y, np.where(self.Y==self.Y[n]))   # n =/= i
+        active_X = active_X[np.where(self.active_particles==1)[0]]
+        active_Y = active_Y[np.where(self.active_particles==1)[0]]
+
+        passive_X = active_X[np.where(self.active_particles==1)[0]]
+        passive_Y = active_Y[np.where(self.active_particles==1)[0]]
         
         v_nx = self.velocities[n, 0]
         v_ny = self.velocities[n, 1]
-        '''
-        v_n = np.array([[v_nx, v_ny],]*(self.nr_particles-1)).transpose()
-        v_n = v_n / np.linalg.norm(v_n)
-        v_n_r_ni = v_n *   
+        
+        v_n_r_ni = v_nx * np.add(np.abs(np.subtract(self.X[n], active_X)), v_ny * np.abs(np.subtract(self.Y[n], active_Y)))   
         r_ni_2 = np.power(np.add(np.abs(np.subtract(self.X[n], active_X)), np.abs(np.subtract(self.Y[n], active_Y))), 2)
         cross = np.subtract(v_nx * (np.abs(np.subtract(self.Y[n], active_Y))), v_ny * np.abs(np.subtract(self.X[n], active_X)))
-        # Cross = Vx * (Yn - Yi) - Vy * (Xn - Xi)
-        T_n = np.multiply(self.T0, np.dot(np.divide(v_n_r_ni, r_ni_2), cross))
-        self.theta[n] += T_n
-        '''
-        v_n = np.array([[v_nx, v_ny],]*(self.nr_particles-1)).transpose()
-        r_ni_2 = np.add(np.power(np.subtract(self.X[n], active_X), 2), np.power(np.subtract(self.Y[n], active_Y), 2))
-        v_n_hat = v_n / np.linalg.norm(v_n)
-        r_ni = np.array([np.subtract(self.X[n], active_X), np.subtract(self.Y[n], active_Y)])
-        r_ni_hat = r_ni / np.linalg.norm(r_ni)
-        cross = np.subtract(v_n_hat[0] * (np.abs(np.subtract(self.Y[n], active_Y))), v_n_hat[1] * np.abs(np.subtract(self.X[n], active_X)))
-        div = np.divide(np.dot(np.transpose(v_n_hat), r_ni_hat), r_ni_2)
-        div[np.isnan(div)] = 0
-        T_n = np.multiply(self.T0, np.dot(div, cross))
-        self.theta[n] += np.sum(T_n)
-            
-    def step(self, TAU=1, SAVEFIG=False, SAVETHRESH=50, LEGEND=True, MSD=False):
+        sum1 = self.T0 * np.sum(np.multiply(np.divide(v_n_r_ni, r_ni_2), cross))
+
+        v_n_r_nm = v_nx * np.add(np.abs(np.subtract(self.X[n], passive_X)), v_ny * np.abs(np.subtract(self.Y[n], passive_Y)))   
+        r_nm_2 = np.power(np.add(np.abs(np.subtract(self.X[n], passive_X)), np.abs(np.subtract(self.Y[n], passive_Y))), 2)
+        cross = np.subtract(v_nx * (np.abs(np.subtract(self.Y[n], passive_Y))), v_ny * np.abs(np.subtract(self.X[n], passive_X)))
+        sum2 = self.T0 * np.sum(np.multiply(np.divide(v_n_r_nm, r_nm_2), cross))
+
+        T_n = sum1 - sum2
+
+        if T_n != 0:
+            self.theta[n] += T_n
+                 
+    def step(self, TAU=1, SAVEFIG=False, SAVETHRESH=50, LEGEND=True, MSD=False, SAVE_DATA=False):
+        self.initialize_inactive_particles()
         for step in range(self.nr_steps):
+            if SAVE_DATA:
+                self.X_history.append(self.X)
+                self.Y_history.append(self.Y)
+            self.inactive_noise(noise_factor=0.01)
             if SAVEFIG and self.time_step == SAVETHRESH+1:
                 plt.savefig('particles_at_50')
             if (self.DRAW):
@@ -143,8 +163,8 @@ class BrownianParticles:
         pop_on_Y = np.where(self.Y == self.Y[i])[0]
         pop_on_site = np.union1d(pop_on_X, pop_on_Y)
         if len(pop_on_site) > 1:
-            return True
-        return False
+            return (True, pop_on_site)
+        return (False, pop_on_site)
     
     def clear_all(self):
         self.historyX = [[] for i in range(0, self.nr_particles)]
@@ -201,25 +221,22 @@ def task1():
         t[i] = i
 
 def task2():
-    brown = BrownianParticles(nr_particles=100, grid_length=100, nr_steps=10000, D_T=0.0052, D_R=0.0002, tau=1, DRAW=True, r_c=100, T0=1)
-    brown.step(SAVEFIG=False, SAVETHRESH=50, LEGEND=False)
+    brown = BrownianParticles(nr_particles=1000, grid_length=100, nr_steps=10, D_T=0.0085, D_R=0.00001, tau=1, DRAW=True, r_c=100, T0=0.75)
+    brown.step(SAVEFIG=False, LEGEND=False, SAVE_DATA=False)
 
-    '''
-    t = np.zeros((brown.nr_steps, 1))
-    for i in range(brown.nr_steps):
-        t[i] = i
-    plt.figure()
-    for i in range(0, brown.nr_particles):
-        plt.loglog(t, brown.historyMSD[i], label='v = ' + str(brown.v[i]) + " \u03BCm/s")
-    plt.legend()
-    plt.xlabel('t')
-    plt.ylabel('MSD [\u03BCm^2]')
-    plt.title('D_T = ' + str(brown.D_T) + 
-                    "      D_R = " + str(brown.D_R) + 
-                    "     Number of Particles = " + 
-                    str(brown.nr_particles))
+def task2_test():
+    brown = BrownianParticles(nr_particles=100, grid_length=100, nr_steps=10, D_T=0.0085, D_R=0.00001, tau=1, DRAW=False, r_c=100, T0=0.75)
+    brown.step(SAVEFIG=False, LEGEND=False, SAVE_DATA=True)
+    #pd.DataFrame(asarray(brown.X_history)).to_csv("HW3/data2/t100000X.csv", header=None)
+    xHist = pd.DataFrame(np.reshape(brown.X_history, (brown.nr_particles*brown.nr_steps, 1)), columns=["colummn"])
+    xHist.to_csv('listX.csv', index=False)
+    yHist = pd.DataFrame(np.reshape(brown.Y_history, (brown.nr_particles*brown.nr_steps, 1)), columns=["colummn"])
+    yHist.to_csv('listY.csv', index=False)
+    #pd.DataFrame(asarray(brown.Y_history)).to_csv("HW3/data2/t100000Y.csv", header=None)
+    X = genfromtxt("listX.csv", delimiter=',')
+    Y = genfromtxt("listY.csv", delimiter=',')
+    plt.plot(X, Y, linestyle='none', marker='o')
     plt.show()
-    '''
 
 #save_data()
 #task1()
--- test.py
+++ test.py
@@ -1,10 +1,3 @@
 import numpy as np
 
-a = np.random.normal(0, 1)
-b = np.array([1, 2 ,3, 3, 5])
-c = np.where(b==3)
-c= c[0]
-print(c[1])
-
-aab = np.array([1, 1, 1, 1])
-print(5*aab)
+print(np.random.randint(0, 4))
